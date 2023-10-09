75ddc864b449c2e34a350951838a9747aaf69f74
zxy <zxy267304@gmail.com>
2023-10-09 03:01:03 +0000

    Add testcase for fork interface.

--- test/main.cc
+++ test/main.cc
@@ -11,6 +11,7 @@
 using primihub::link::Channel;
 using primihub::link::MemoryChannel;
 using primihub::link::retcode;
+using primihub::link::Status;
 
 static std::string gen_random(uint32_t len, uint32_t seed) {
   static const char alphanum[] = "0123456789"
@@ -98,4 +99,54 @@ TEST(channel, multiple_test) {
   EXPECT_EQ(recv_str1, str1);
   EXPECT_EQ(recv_str2, str2);
   EXPECT_EQ(recv_str3, str3);
+}
+
+TEST(channel, fork_test) {
+  uint16_t fork_num = 10;
+  std::vector<std::shared_ptr<Channel>> client_fork_channels;
+  std::vector<std::shared_ptr<Channel>> server_fork_channels;
+
+  auto channel_impl = std::make_shared<MemoryChannel>();
+  auto channel = std::make_shared<Channel>(channel_impl, "fork_test");
+
+  for (uint16_t i = 0; i < fork_num; i++) {
+    auto new_channel = channel->fork();
+    client_fork_channels.push_back(new_channel);
+    server_fork_channels.push_back(new_channel);
+  }
+
+  std::string send_buf = gen_random(1024, 10);
+  auto send_fn = [&client_fork_channels, &send_buf]() {
+    std::vector<Status> send_status;
+
+    for (auto &channel : client_fork_channels)
+      send_status.push_back(channel->asyncSend(send_buf.data(), send_buf.size()));
+
+    for (auto &status : send_status)
+      EXPECT_EQ(status.IsOK(), true);
+  };
+
+  auto recv_fn = [&server_fork_channels, &send_buf]() {
+    std::vector<std::string> all_recv_buf;
+    all_recv_buf.resize(server_fork_channels.size());
+    for (auto &recv_buf : all_recv_buf)
+      recv_buf.resize(1024);
+
+    std::vector<std::future<Status>> recv_futs;
+    for (size_t i = 0; i < server_fork_channels.size(); i++)
+      recv_futs.push_back(server_fork_channels[i]->asyncRecv(
+          all_recv_buf[i].data(), all_recv_buf[i].size()));
+
+    for (auto &fut : recv_futs)
+      EXPECT_EQ(fut.get().IsOK(), true);
+
+    for (auto &recv_buf : all_recv_buf)
+      EXPECT_EQ(recv_buf == send_buf, true);
+  };
+
+  std::future<void> recv_fut = std::async(recv_fn);
+  std::future<void> send_fut = std::async(send_fn);
+
+  send_fut.get();
+  recv_fut.get();
 }
