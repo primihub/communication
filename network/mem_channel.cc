0914a023f7602442ef4b2c1384334fde89491946
Xiangyu Zhang <zxy267304@gmail.com>
2023-10-13 15:51:42 +0800

    Add 'fork' method to 'Channel' (#1)
    * Begin to add fork interface to generate new channel based on current channel.
    
    * Fix mismatch error during multiple send then recv.
    
    * Add testcase for fork interface.
    
    * Two memory channel with the same queue.
    
    * The 'asyncRecv' function always returns ok even when an error occurs, fix it.
    
    * The 'MemoryChannel' now supports both send and receive operations.

--- network/mem_channel.cc
+++ network/mem_channel.cc
@@ -1,54 +1,189 @@
 /*
-* Copyright (c) 2023 by PrimiHub
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      https://www.apache.org/licenses/
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (c) 2023 by PrimiHub
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include "network/mem_channel.h"
+#include "common/threadsafe_queue.h"
+
+#include <cstring>
 #include <iostream>
 #include <string_view>
-#include <cstring>
 
 namespace primihub::link {
-retcode MemoryChannel::SendImpl(const std::string& send_buf) {
-  auto send_sv = std::string_view(send_buf.data(), send_buf.size());
-  SendImpl(send_sv);
-  return retcode::SUCCESS;
+namespace {
+class QueuePair {
+public:
+  QueuePair() {
+    this->queue_c2s_ = std::make_shared<ThreadSafeQueue<std::string>>();
+    this->queue_s2c_ = std::make_shared<ThreadSafeQueue<std::string>>();
+  }
+
+  ThreadSafeQueuePtr getQueue(bool c2s) {
+    if (c2s)
+      return queue_c2s_;
+    else
+      return queue_s2c_;
+  }
+
+private:
+  ThreadSafeQueuePtr queue_c2s_;
+  ThreadSafeQueuePtr queue_s2c_;
+};
+
+class QueueManager {
+public:
+  QueueManager() = default;
+  std::shared_ptr<QueuePair> getOrCreate(const std::string &key) {
+    std::lock_guard<std::mutex> lock(map_mu_);
+    auto iter = queue_map_.find(key);
+    if (iter != queue_map_.end())
+      return iter->second;
+
+    std::shared_ptr<QueuePair> queue = std::make_shared<QueuePair>();
+    queue_map_.insert(std::make_pair(key, queue));
+
+    return queue;
+  }
+
+private:
+  std::map<std::string, std::shared_ptr<QueuePair>> queue_map_;
+  std::mutex map_mu_;
+};
+
+QueueManager manager;
+} // namespace
+
+MemoryChannel::MemoryChannel(MemoryChannel::ChannelRole role) {
+  this->role_ = role;
 }
+
+MemoryChannel::MemoryChannel(const std::string &key,
+                             MemoryChannel::ChannelRole role) {
+  this->key_ = key;
+  this->role_ = role;
+
+  std::shared_ptr<QueuePair> queue = manager.getOrCreate(this->key_);
+  storage_c2s_ = queue->getQueue(true);
+  storage_s2c_ = queue->getQueue(false);
+}
+
+void MemoryChannel::SetKey(const std::string &key) {
+  this->key_ = key;
+
+  std::shared_ptr<QueuePair> queue = manager.getOrCreate(this->key_);
+  storage_c2s_ = queue->getQueue(true);
+  storage_s2c_ = queue->getQueue(false);
+}
+
 retcode MemoryChannel::SendImpl(std::string_view send_buff_sv) {
-  this->storage.clear();
-  this->storage.append(send_buff_sv.data(), send_buff_sv.size());
+  ThreadSafeQueuePtr storage = nullptr;
+  if (role_ == ChannelRole::SERVER)
+    storage = storage_s2c_;
+  else
+    storage = storage_c2s_;
+
+  storage->push(std::string(send_buff_sv.data(), send_buff_sv.size()));
+
+  if (VLOG_IS_ON(8)) {
+    std::string send_data;
+    for (const auto &ch : send_buff_sv) {
+      send_data.append(std::to_string(static_cast<int>(ch))).append(" ");
+    }
+    LOG(INFO) << "MemoryChannel::SendImpl "
+              << "send_key: " << key_ << " "
+              << "data size: " << send_buff_sv.size();
+    // << "send data: [" << send_data << "]";
+  }
+
   return retcode::SUCCESS;
 }
 
-retcode MemoryChannel::SendImpl(const char* buff, size_t size) {
+retcode MemoryChannel::SendImpl(const std::string &send_buf) {
+  auto send_sv = std::string_view(send_buf.data(), send_buf.size());
+  return SendImpl(send_sv);
+}
+
+retcode MemoryChannel::SendImpl(const char *buff, size_t size) {
   auto send_sv = std::string_view(buff, size);
-  SendImpl(send_sv);
+  return SendImpl(send_sv);
+}
+
+retcode MemoryChannel::RecvImpl(std::string *recv_buf) {
+  ThreadSafeQueuePtr storage = nullptr;
+  if (role_ == ChannelRole::SERVER)
+    storage = storage_c2s_;
+  else
+    storage = storage_s2c_;
+
+  std::string data_buf;
+  storage->wait_and_pop(data_buf);
+  *recv_buf = std::move(data_buf);
+
+  if (VLOG_IS_ON(8)) {
+    std::string recv_data;
+    for (const auto &ch : *recv_buf) {
+      recv_data.append(std::to_string(static_cast<int>(ch))).append(" ");
+    }
+
+    LOG(INFO) << "MemoryChannel::RecvImpl "
+              << "recv_key: " << key_ << " data size: " << recv_buf->size();
+    // << " recv data: " << recv_data;
+  }
+
   return retcode::SUCCESS;
 }
 
-retcode MemoryChannel::RecvImpl(std::string* recv_buf) {
-  *recv_buf = this->storage;
+retcode MemoryChannel::RecvImpl(char *recv_buf, size_t recv_size) {
+  ThreadSafeQueuePtr storage = nullptr;
+  if (role_ == ChannelRole::SERVER)
+    storage = storage_c2s_;
+  else
+    storage = storage_s2c_;
+
+  std::string tmp_recv_buf;
+  storage->wait_and_pop(tmp_recv_buf);
+  if (tmp_recv_buf.size() != recv_size) {
+    LOG(ERROR) << "data length does not match: "
+               << " "
+               << "expected: " << recv_size << " "
+               << "actually: " << tmp_recv_buf.size();
+    return retcode::FAIL;
+  }
+
+  memcpy(recv_buf, tmp_recv_buf.data(), recv_size);
+
+  if (VLOG_IS_ON(8)) {
+    std::string recv_data;
+    for (const auto &ch : tmp_recv_buf) {
+      recv_data.append(std::to_string(static_cast<int>(ch))).append(" ");
+    }
+
+    LOG(INFO) << "MemoryChannel::RecvImpl "
+              << "recv_key: " << key_ << " "
+              << "data size: " << recv_size;
+    // << "recv data: [" << recv_data << "] ";
+  }
+
   return retcode::SUCCESS;
 }
 
-retcode MemoryChannel::RecvImpl(char* recv_buf, size_t recv_size) {
-  memcpy(recv_buf, this->storage.data(), this->storage.size());
-  return retcode::SUCCESS;
+std::shared_ptr<ChannelBase> MemoryChannel::ForkImpl(const std::string &key) {
+  return std::make_shared<MemoryChannel>(key, this->role_);
 }
-void MemoryChannel::close() {
 
-}
-void MemoryChannel::cancel() {
+void MemoryChannel::close() {}
 
-}
-}  // namespace primihub::link
+void MemoryChannel::cancel() {}
+
+} // namespace primihub::link
--- test/main.cc
+++ test/main.cc
@@ -1,55 +1,197 @@
 #include <glog/logging.h>
+#include <gtest/gtest.h>
 
+#include <array>
 #include <iostream>
 #include <vector>
-#include <array>
-#include "network/mem_channel.h"
-#include "network/channel_interface.h"
-int main() {
-  // create channel
-  auto channel_impl = std::make_shared<primihub::link::MemoryChannel>();
-  auto channel = std::make_shared<primihub::link::Channel>(channel_impl);
-  // test send recv string
-  LOG(INFO) << "test send recv string";
-  std::string buf = "Hello World";
-  channel->asyncSend(buf);
-  std::string recv_buf;
-  channel->asyncRecv(recv_buf).get();
-  LOG(INFO) << "recv string content: " << recv_buf;
-  // test send recv arary
-  LOG(INFO) << "test send recv arary";
-  std::array<int64_t, 2> shape{1, 1};
-  std::string send_item;
-  for (const auto& item: shape) {
-    send_item.append(std::to_string(item)).append(" ");
-  }
-  LOG(INFO) << "origin item: " << send_item;
-  channel->asyncSend(shape);
-  std::array<int64_t, 2> recv_shape;
-  auto fut = channel->asyncRecv(recv_shape);
-  fut.get();
-  std::string recv_item;
-  for (const auto& item : recv_shape) {
-    recv_item.append(std::to_string(item)).append(" ");
-  }
-  LOG(INFO) << "recv array item: " << recv_item;
 
-  // test send recv vector
-  LOG(INFO) << "test send recv vector";
+#include "network/channel_interface.h"
+#include "network/mem_channel.h"
+
+using primihub::link::Channel;
+using primihub::link::MemoryChannel;
+using primihub::link::retcode;
+using primihub::link::Status;
+
+using ChannelRole = MemoryChannel::ChannelRole;
+
+static std::string gen_random(uint32_t len, uint32_t seed) {
+  static const char alphanum[] = "0123456789"
+                                 "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                 "abcdefghijklmnopqrstuvwxyz";
+  std::string tmp_s;
+  tmp_s.reserve(len);
+  
+  srand(seed);
+  for (uint32_t i = 0; i < len; ++i)
+    tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
+
+  return tmp_s;
+}
+
+TEST(channel, type_test) {
+  auto channel_impl1 = std::make_shared<MemoryChannel>(ChannelRole::CLIENT);
+  auto channel1 = std::make_shared<Channel>(channel_impl1, "type_test");
+
+  auto channel_impl2 = std::make_shared<MemoryChannel>(ChannelRole::SERVER);
+  auto channel2 = std::make_shared<Channel>(channel_impl2, "type_test");
+
+  std::string buf = "Hello World";
+  channel1->asyncSend(buf);
+  std::string recv_buf;
+  channel2->asyncRecv(recv_buf).get();
+
+  EXPECT_EQ(buf, recv_buf);
+
+  std::array<int64_t, 2> shape{1, 1};
+  channel1->asyncSend(shape);
+
+  std::array<int64_t, 2> recv_shape;
+  auto fut = channel2->asyncRecv(recv_shape);
+  fut.get();
+
+  EXPECT_EQ(shape[0], recv_shape[0]);
+  EXPECT_EQ(shape[1], recv_shape[1]);
+
   std::vector<int> vec{1, 2, 3, 4};
-  std::string send_vec_item;
-  for (const auto& item: vec) {
-    send_vec_item.append(std::to_string(item)).append(" ");
-  }
-  LOG(INFO) << "origin vector item: " << send_vec_item;
-  channel->asyncSend(vec);
+  channel1->asyncSend(vec);
+
   std::vector<int> recv_vec;
   recv_vec.resize(vec.size());
-  channel->asyncRecv(recv_vec).get();
-  std::string recv_vec_item;
-  for (const auto& item : recv_vec) {
-    recv_vec_item.append(std::to_string(item)).append(" ");
+  channel2->asyncRecv(recv_vec).get();
+
+  EXPECT_EQ(vec.size(), recv_vec.size());
+  for (size_t i = 0; i < recv_vec.size(); i++)
+    EXPECT_EQ(vec[i], recv_vec[i]);
+}
+
+TEST(channel, multiple_test) {
+  std::string str1 = gen_random(1024, 1);
+  std::string str2 = gen_random(2048, 2);
+  std::string str3 = gen_random(4096, 3);
+
+  auto channel_impl1 = std::make_shared<MemoryChannel>(ChannelRole::CLIENT);
+  auto channel1 = std::make_shared<Channel>(channel_impl1, "type_test");
+
+  auto channel_impl2 = std::make_shared<MemoryChannel>(ChannelRole::SERVER);
+  auto channel2 = std::make_shared<Channel>(channel_impl2, "type_test");
+
+  auto status = channel1->asyncSend(str1);
+  EXPECT_EQ(status.IsOK(), true);
+
+  status = channel1->asyncSend(str2);
+  EXPECT_EQ(status.IsOK(), true);
+
+  status = channel1->asyncSend(str3);
+  EXPECT_EQ(status.IsOK(), true);
+
+  std::string recv_str1;
+  std::string recv_str2;
+  std::string recv_str3;
+
+  {
+    auto fut = channel2->asyncRecv(recv_str1);
+    fut.get();
   }
-  LOG(INFO) << "recv vector item: " << recv_vec_item;
-  return 0;
+
+  {
+    auto fut = channel2->asyncRecv(recv_str2);
+    fut.get();
+  }
+
+  {
+    auto fut = channel2->asyncRecv(recv_str3);
+    fut.get();
+  }
+
+  EXPECT_EQ(recv_str1, str1);
+  EXPECT_EQ(recv_str2, str2);
+  EXPECT_EQ(recv_str3, str3);
+
+  status = channel2->asyncSend(str1);
+  EXPECT_EQ(status.IsOK(), true);
+
+  status = channel2->asyncSend(str2);
+  EXPECT_EQ(status.IsOK(), true);
+  
+  status = channel2->asyncSend(str3);
+  EXPECT_EQ(status.IsOK(), true);
+  
+  recv_str1.clear();
+  recv_str2.clear();
+  recv_str3.clear();
+
+  {
+    auto fut = channel1->asyncRecv(recv_str1);
+    fut.get();
+  }
+
+  {
+    auto fut = channel1->asyncRecv(recv_str2);
+    fut.get();
+  }
+
+  {
+    auto fut = channel1->asyncRecv(recv_str3);
+    fut.get();
+  }
+
+  EXPECT_EQ(recv_str1, str1);
+  EXPECT_EQ(recv_str2, str2);
+  EXPECT_EQ(recv_str3, str3);
+}
+
+TEST(channel, fork_test) {
+  uint16_t fork_num = 10;
+  std::vector<std::shared_ptr<Channel>> client_fork_channels;
+  std::vector<std::shared_ptr<Channel>> server_fork_channels;
+
+  auto channel_impl1 = std::make_shared<MemoryChannel>(ChannelRole::CLIENT);
+  auto channel1 = std::make_shared<Channel>(channel_impl1, "fork_test");
+
+  auto channel_impl2 = std::make_shared<MemoryChannel>(ChannelRole::SERVER);
+  auto channel2 = std::make_shared<Channel>(channel_impl2, "fork_test");
+
+  for (uint16_t i = 0; i < fork_num; i++) {
+    auto new_channel = channel1->fork();
+    client_fork_channels.push_back(new_channel);
+    
+    new_channel = channel2->fork(); 
+    server_fork_channels.push_back(new_channel);
+  }
+
+  std::string send_buf = gen_random(1024, 10);
+  auto send_fn = [&client_fork_channels, &send_buf]() {
+    std::vector<Status> send_status;
+
+    for (auto &channel : client_fork_channels)
+      send_status.push_back(channel->asyncSend(send_buf.data(), send_buf.size()));
+
+    for (auto &status : send_status)
+      EXPECT_EQ(status.IsOK(), true);
+  };
+
+  auto recv_fn = [&server_fork_channels, &send_buf]() {
+    std::vector<std::string> all_recv_buf;
+    all_recv_buf.resize(server_fork_channels.size());
+    for (auto &recv_buf : all_recv_buf)
+      recv_buf.resize(1024);
+
+    std::vector<std::future<Status>> recv_futs;
+    for (size_t i = 0; i < server_fork_channels.size(); i++)
+      recv_futs.push_back(server_fork_channels[i]->asyncRecv(
+          all_recv_buf[i].data(), all_recv_buf[i].size()));
+
+    for (auto &fut : recv_futs)
+      EXPECT_EQ(fut.get().IsOK(), true);
+
+    for (auto &recv_buf : all_recv_buf)
+      EXPECT_EQ(recv_buf == send_buf, true);
+  };
+
+  std::future<void> recv_fut = std::async(recv_fn);
+  std::future<void> send_fut = std::async(send_fn);
+
+  send_fut.get();
+  recv_fut.get();
 }
